9db5d82d2fc175260f92183d79079c3cdca16807
Haidepptrai <hainguyenhoangcontact@gmail.com>
2026-05-16 14:20:19 +0700

    update: initial setup

--- backend/MyApplication.WebApi/Controllers/WeatherForecastController.cs
+++ backend/MyApplication.WebApi/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using MyApplication.Domain;
 
 namespace MyApplication.WebApi.Controllers;
 
@@ -12,14 +13,16 @@ public class WeatherForecastController : ControllerBase
     ];
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    public ActionResult<IEnumerable<WeatherForecast>> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        var response = Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
         })
-        .ToArray();
+            .ToArray();
+
+        return Ok(response);
     }
 }
--- backend/MyApplication.WebApi/Program.cs
+++ backend/MyApplication.WebApi/Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.OpenApi;
+
 namespace MyApplication.WebApi;
 
 public class Program
@@ -11,14 +13,30 @@ public class Program
 
         builder.Services.AddControllers();
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-        builder.Services.AddOpenApi();
+        builder.Services.AddEndpointsApiExplorer();
+
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "My Web API",
+                Version = "v1",
+                Description = "My .NET 10 Web API",
+            });
+        });
+
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
-            app.MapOpenApi();
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+                options.RoutePrefix = string.Empty; // Open Swagger at root URL
+            });
         }
 
         app.UseHttpsRedirection();
